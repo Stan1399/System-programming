5c507eb306834967bd8b3500a1924db361cbf41c
Stanis <ctaniclav-1999@tut.by>
2020-04-13 20:45:14 +0300

    All labs from the last sem

---
+++ L2/L2/L2.cpp
@@ -0,0 +1,120 @@
+﻿// L2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <stdlib.h>
+#include <locale.h>  
+#include <stdio.h>
+#include <windows.h>
+#include <tchar.h>
+
+
+void ErrorMes(LPTSTR lpszFunction)
+{
+
+	LPVOID lpMsgBuf;
+	LPVOID lpDisplayBuf;
+	DWORD dw = GetLastError();
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dw,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL);
+
+
+	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+	wprintf(L"%s failed with error %d: %s",
+		lpszFunction, dw, lpMsgBuf);
+
+	LocalFree(lpMsgBuf);
+	LocalFree(lpDisplayBuf);
+
+}
+
+BOOL FindFileByClaster(TCHAR* volume) {
+
+	HANDLE hDevice = CreateFile(volume,  
+		GENERIC_READ,                       
+		FILE_SHARE_READ | FILE_SHARE_WRITE,
+		NULL,           
+		OPEN_EXISTING, 
+		FILE_FLAG_BACKUP_SEMANTICS,
+		NULL);
+
+	if (hDevice == INVALID_HANDLE_VALUE)
+	{
+		ErrorMes((LPTSTR)L"CreateFile");
+		return FALSE;
+	}
+
+	STARTING_VCN_INPUT_BUFFER input = { 0 };
+	input.StartingVcn.QuadPart = 0L;
+	// input.Cluster[0].QuadPart = cluster;
+
+	DWORD output[5000] = {};
+	DWORD dwRes = 0;
+	RETRIEVAL_POINTERS_BUFFER  result = { 0 };
+
+	BOOL bRes = DeviceIoControl((HANDLE)hDevice,	// handle to file, directory, or volume 
+		FSCTL_GET_RETRIEVAL_POINTERS,				// dwIoControlCode
+		(LPVOID)&input,								// input buffer 
+		(DWORD)sizeof(input),						// size of input buffer 
+		(LPVOID)output,							// output buffer 
+		(DWORD)5000,								// size of output buffer 
+		(LPDWORD)&dwRes,							// number of bytes returned 
+		NULL);										// OVERLAPPED structure
+
+	if (bRes == FALSE) {
+		ErrorMes((LPTSTR)L"DeviceIoControl");
+		return FALSE;
+	}
+
+	memcpy(&result, &output, sizeof(RETRIEVAL_POINTERS_BUFFER));
+
+	if (result.ExtentCount == 0) {
+		wprintf(L"File not found\n");
+		return FALSE;
+	}
+	wprintf(L"Extent count: %d\n", result.ExtentCount);
+	wprintf(L"File information:\n");
+	wprintf(L"\tStart cluster: %llu ", result.Extents[0].Lcn.QuadPart);
+	wprintf(L"\r\n\tLength: %llu \n\n", result.Extents[0].NextVcn.QuadPart - result.StartingVcn.QuadPart);
+	for (int i = 1; i < result.ExtentCount; i++)
+	{
+		wprintf(L"File information:\n");
+		wprintf(L"\tStart cluster: %llu ", result.Extents[i].Lcn.QuadPart);
+		wprintf(L"\r\n\tLength: %llu \n\n", result.Extents[i].NextVcn.QuadPart - result.Extents[i-1].NextVcn.QuadPart);
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	TCHAR inp[30];
+	/*wprintf(L"Введите путь к каталогу: \n");
+	scanf_s("%s", inp, 30);
+	printf("%s", inp);
+	FindFileByClaster(inp);*/
+	FindFileByClaster((TCHAR*)L"E:\\Games\\Spore\\SporeBin\\SporeApp.exe");
+
+	system("PAUSE");
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ L8/L8/L8.cpp
@@ -0,0 +1,324 @@
+﻿// L8.cpp : Defines the entry point for the application.
+//
+
+#include "stdafx.h"
+#include "L8.h"
+
+#define MAX_LOADSTRING 100
+
+// Global Variables:
+HINSTANCE hInst;                                // current instance
+WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+void				InitControls(HWND);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPTSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Place code here.
+	MSG msg;
+	HACCEL hAccelTable;
+
+	// Initialize global strings
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_L8, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Perform application initialization:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_L8));
+
+	// Main message loop:
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDC_L8));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassEx(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	HWND hWnd;
+
+	hInst = hInstance; // Store instance handle in our global variable
+
+	hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+	InitControls(hWnd);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+#define BUTTON_DRAW_ID 1
+#define BUTTON_CLEAR_ID 2
+
+void InitControls(HWND hWnd) {
+	HWND buttonAdd = CreateWindow(L"BUTTON", NULL, WS_VISIBLE | WS_CHILD | BS_OWNERDRAW | BS_PUSHBUTTON,
+		20, 520, 200, 200, hWnd, (HMENU)BUTTON_DRAW_ID, NULL, NULL);
+	HWND buttonClear = CreateWindow(L"BUTTON", NULL, WS_VISIBLE | WS_CHILD | BS_OWNERDRAW | BS_PUSHBUTTON,
+		290, 520, 200, 200, hWnd, (HMENU)BUTTON_CLEAR_ID, NULL, NULL);
+}
+
+bool pictureVisible = false;
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE:  Processes messages for the main window.
+//
+//  WM_COMMAND	- process the application menu
+//  WM_PAINT	- Paint the main window
+//  WM_DESTROY	- post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	int wmId, wmEvent, i;
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// Parse the menu selections:
+		switch (wmId)
+		{
+		case BUTTON_DRAW_ID:
+			pictureVisible = true;
+			InvalidateRect(hWnd, NULL, true);
+			break;
+		case BUTTON_CLEAR_ID:
+			pictureVisible = false;
+			InvalidateRect(hWnd, NULL, true);
+			break;
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+		break;
+	case WM_DRAWITEM:
+	{
+		DRAWITEMSTRUCT *draw = (DRAWITEMSTRUCT*)lParam;
+		HBRUSH brush;
+		HPEN pen;
+
+		switch (draw->CtlID)
+		{
+		case BUTTON_DRAW_ID:
+		{
+			HDC hdc = draw->hDC;
+
+			brush = CreateSolidBrush(RGB(120, 130, 12));
+
+			/*if (draw->itemAction == ODA_SELECT) {
+				brush = CreateSolidBrush(RGB(120, 130, 12));
+			}
+			else {
+				brush = CreateSolidBrush(RGB(0, 42, 224));
+			}*/
+
+
+			pen = CreatePen(BS_SOLID, 2, RGB(0, 0, 0));
+
+			SelectObject(hdc, brush);
+			SelectObject(hdc, pen);
+
+			Ellipse(hdc, 0, 0, 200, 200);
+			// MoveToEx(hdc, 10, 30, NULL);
+			// MoveToEx(hdc, 30, 10, NULL);
+		}
+		break;
+		case BUTTON_CLEAR_ID:
+		{
+			HDC hdc = draw->hDC;
+
+			brush = CreateSolidBrush(RGB(4, 100, 55));
+
+			/*if (draw->itemAction == ODA_SELECT) {
+				brush = CreateSolidBrush(RGB(0, 0, 255));
+			}
+			else {
+				brush = CreateSolidBrush(RGB(255, 0, 0));
+			}*/
+			pen = CreatePen(BS_SOLID, 2, RGB(0, 0, 0));
+
+			SelectObject(hdc, brush);
+			SelectObject(hdc, pen);
+
+			// Ellipse(hdc, 0, 0, 60, 60);
+			MoveToEx(hdc, 0, 0, NULL);
+			LineTo(hdc, 200, 200);
+			MoveToEx(hdc, 200, 0, NULL);
+			LineTo(hdc, 0, 200);
+		}
+		break;
+		default:
+			break;
+		}
+
+	}
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code here...
+
+		HBRUSH brush;
+		HPEN pen;
+		if (pictureVisible)
+		{
+
+			brush = CreateSolidBrush(RGB(0, 225, 0));
+			SelectObject(hdc, brush);
+			pen = CreatePen(BS_SOLID, 2, RGB(0, 12, 0));
+			SelectObject(hdc, pen);
+
+			POINT p[18] = { 1000, 12, 132, 120, 1112, 35, 23, 14,
+				123, 421, 321, 342, 12, 342, 123, 112,
+				422, 123, 580, 290, 245, 270, 537, 270,
+				734, 290, 123, 290, 260, 213, 342, 270,
+				160, 250, 1200, 560 };
+			Polygon(hdc, p, 18);
+			
+			
+			pen = CreatePen(BS_SOLID, 1, RGB(0, 0, 0));
+			SelectObject(hdc, pen);
+			brush = CreateSolidBrush(RGB(0, 0, 242));
+			SelectObject(hdc, brush);
+
+			POINT p2[3] = { 123, 142, 740, 236, 835, 523 };
+			Polygon(hdc, p2, 3);
+
+			/*pen = CreatePen(BS_SOLID, 2, RGB(0, 0, 0));
+			SelectObject(hdc, pen);
+
+			MoveToEx(hdc, 0, 40, NULL);
+			LineTo(hdc, 820, 40);*/
+			
+			pen = CreatePen(BS_SOLID, 5, RGB(254, 45, 0));
+			SelectObject(hdc, pen);
+
+			MoveToEx(hdc, 234, 40, NULL);
+			LineTo(hdc, 345, 12);
+			LineTo(hdc, 23, 23);
+
+			MoveToEx(hdc, 0, 342, NULL);
+			LineTo(hdc, 1000, 231);
+
+			pen = CreatePen(BS_SOLID, 7, RGB(0, 0, 255));
+			SelectObject(hdc, pen);
+
+			Arc(hdc, -620, 200, 660, 760, 420, 280, 20, 200);
+			Arc(hdc, 360, 230, 700, 490, 560, 230, 360, 200);
+
+			brush = CreateSolidBrush(RGB(100, 50, 50));
+			SelectObject(hdc, brush);
+
+			Ellipse(hdc, 60, 200, 160, 300);
+			Ellipse(hdc, 170, 210, 270, 310);
+			Ellipse(hdc, 280, 220, 380, 320);
+			Ellipse(hdc, 390, 230, 490, 330);
+
+		}
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
